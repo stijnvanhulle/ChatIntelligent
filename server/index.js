e750e759a3f7b02f6bf73830c9ad1228c1e32960
Stijn Van hulle <stijnvanhulle@macbook-2.local>
2016-12-21 23:54:15 +0100

    Change for heroku

--- server/index.js
+++ server/index.js
@@ -3,11 +3,9 @@
 * @Date:   2016-12-02T09:44:31+01:00
 * @Email:  me@stijnvanhulle.be
 * @Last modified by:   stijnvanhulle
-* @Last modified time: 2016-12-21T23:47:22+01:00
+* @Last modified time: 2016-12-21T23:53:44+01:00
 * @License: stijnvanhulle.be
 */
-
-
 
 require(`dotenv`).load();
 const pluginHandler = require(`./lib/pluginHandler`);
@@ -15,9 +13,9 @@ const path = require(`path`);
 const mongoose = require("mongoose");
 const Server = require('hapi').Server;
 //const WebpackPlugin = require('hapi-webpack-plugin');
-const port = process.env.PORT || 3000;
-const mongo=process.env.mongo|| '51.254.211.109';
-const {version}= require('../package.json');
+const port = process.env.PORT;
+const mongo = process.env.mongo || '51.254.211.109:27017';
+const {version} = require('../package.json');
 
 const server = new Server({
   connections: {
@@ -29,8 +27,6 @@ const server = new Server({
   }
 });
 
-
-
 const mongodb = {
   options: {
     db: {
@@ -40,9 +36,8 @@ const mongodb = {
       poolSize: 5
     }
   },
-  uri: 'mongodb://'+ mongo +':3001/chat'
+  uri: 'mongodb://' + mongo + '/chat'
 };
-
 
 server.connection({port});
 server.register(require(`./modules/`), pluginHandler);
@@ -50,9 +45,9 @@ server.register(require(`./routes/`), pluginHandler);
 
 const startServer = () => {
   mongoose.connect(mongodb.uri, mongodb.options, (err) => {
-    if (err){
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       server.start(err => {
         if (err)
           console.error(err);
