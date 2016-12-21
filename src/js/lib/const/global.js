07ad2f4c13b5154eb272204f76e1aea4be765722
Stijn Van hulle <stijnvanhulle@macbook-2.local>
2016-12-22 00:13:38 +0100

    https socket

--- src/js/lib/const/global.js
+++ src/js/lib/const/global.js
@@ -3,18 +3,18 @@
 * @Date:   2016-12-15T15:13:22+01:00
 * @Email:  me@stijnvanhulle.be
 * @Last modified by:   stijnvanhulle
-* @Last modified time: 2016-12-19T23:06:23+01:00
+* @Last modified time: 2016-12-22T00:13:31+01:00
 * @License: stijnvanhulle.be
 */
 import EventEmitter from 'events';
 
 class Emitter extends EventEmitter {}
 
-const url = `http://localhost:3000`;
-
+const url = `https://localhost:3000`;
 const global = {
   socket: {},
   events: new Emitter(),
   url: url
 };
+
 module.exports = global;
